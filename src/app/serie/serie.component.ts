f069fd9f85a34b2972e1896f0c353400b468cceb
Felipe <f.sanmiguel@uniandes.edu.co>
2025-04-21 23:39:28 -0500

    Card terminada

--- src/app/serie/serie.component.ts
+++ src/app/serie/serie.component.ts
@@ -1,24 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { Serie } from './serie';
 import { dataSeries } from './dataSeries';
-import { CommonModule, NgFor } from '@angular/common';
+import { CommonModule, NgFor, NgIf } from '@angular/common';
 import { SerieService } from './serie.service';
 
 @Component({
   selector: 'app-serie',
   templateUrl: './serie.component.html',
   styleUrls: ['./serie.component.css'],
-  imports: [NgFor]
+  imports: [NgFor,NgIf]
 })
 export class SerieComponent implements OnInit {
 
   series: Array<Serie> = [];
   promedio: number = 0;
-  
+  selectedSerie: Serie | null = null;
+
+
   constructor(private serieService: SerieService) { }
 
   getSeriesList(): Array<Serie> {
     return dataSeries;
+  }
+
+  selectSerie(serie: Serie): void {
+    this.selectedSerie = serie;
   }
 
   calcularPromedio() {
